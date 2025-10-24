dcbe5491ec30c694496c6ace731d92acbcefa113
Felipe <f.rojasd@uniandes.edu.co>
2025-10-24 09:43:36 -0500

    first commit

---
+++ ClientesEmisores.java
@@ -0,0 +1,19 @@
+public class ClientesEmisores extends Thread {
+    private int numCorreos;
+    private BuzonEntrada buzonEntrada;
+
+    public ClientesEmisores(int Correos, BuzonEntrada Entrada){
+        this.numCorreos = Correos;
+        this.buzonEntrada = Entrada;
+    }
+
+    public void run(){
+        System.out.println("== Mensaje de inicio ==");
+    }
+
+    
+
+
+
+    
+}
